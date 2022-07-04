5caa87625398578c8077eb6e1058ef7fef9ba75b
talkingpotatover2 <gjdlwls95@gmail.com>
2022-07-04 17:49:57 +0900

    create list.json ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Nav extends Component {
+  state = {
+    list:[]
+  }
+  componentDidMount(){  //오버라이딩
+    fetch('list.json')  //public이 기준점
+    // .then(function(result){
+    //   return result.json();
+    // })
+    // .then((result)=>result.json())  //return을 의미함(return이 생략됨)
+    .then(function(result){
+      return result.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      this.setState({list:json})
+    }.bind(this));
+  }
+  render(){
+    let listTag = [];
+    for(let i=0; i<this.state.list.length; i++){
+      let li = this.state.list[i]
+      listTag.push(<li key={li.id}>
+        <a href={li.id} data-id={li.id}
+          onClick={function(e){
+            e.preventDefault();
+            this.props.onClick(e.target.dataset.id);
+          }.bind(this)}
+        >{li.title}</a></li>)
+    }
+    return(
+      <nav>
+        <ul>
+          {listTag}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component {
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    article:{title:'Welcome', desc:'Hello, React & Ajax'}
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>web</h1>
+        <Nav onClick={function(id){
+          fetch(id+'.json')
+          .then(function(result){
+            return result.json();
+          })
+          .then(function(json){
+            this.setState({
+              article:{title:json.title, desc:json.desc}
+            })
+          }.bind(this))
+        }.bind(this)}></Nav>
+        <Article 
+          title={this.state.article.title}
+          desc={this.state.article.desc}
+        ></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
